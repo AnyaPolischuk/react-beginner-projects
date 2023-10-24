2ed7ee096dd94b697895d73766ae7ad3e3bae159
a.polischuk <a.polischuk@adriver.ru>
2023-10-24 14:25:16 +0300

    feat: add logic to photo gallery

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Collection } from './Collection';
 import './index.scss';
 
+const categories = [
+  {name: "Все"},
+  {name: "Море"},
+  {name: "Горы"},
+  {name: "Архитектура"},
+  {name: "Города"}
+]
+
 function App() {
+  const [collections, setCollections] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [categoryId, setCategoryId] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setIsLoading(true);
 
-  const onClickPlus = () => {
-    setCount(count + 1);
-  }
+    const category = categoryId ? `category=${categoryId}` : "";
 
-  const onClickMinus = () => {
-    setCount(count - 1);
-  }
+    fetch(`https://65367ee1bb226bb85dd23784.mockapi.io/photo_collection?page=${page}&limit=2&${category}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении данных");
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- Минус</button>
-        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {categories.map((obj, index) => <li onClick={() => setCategoryId(index)} className={categoryId === index ? "active" : ""} key={obj.name}>{obj.name}</li>)}
+        </ul>
+        <input value={searchValue} onChange={event => setSearchValue(event.target.value)} className="search-input" placeholder="Поиск по названию" />
       </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Идет загрузка</h2>
+        ) : (
+          collections
+        .filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+        .map((obj, index) => (
+          <Collection key={index} name={obj.name} images={obj.photos}/>))
+        )}
+      </div>
+      <ul className="pagination">
+        {
+          [...Array(5)].map((_, index) => <li onClick={() => setPage(index + 1)} className={page === index + 1 ? "active" : ""}>{index + 1}</li>)
+        }
+      </ul>
     </div>
   );
 }
